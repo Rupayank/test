3bdac48c556c70b736ccfb8fb89b0ab06e87e9fd
Rupayan <59760234+Rupayank@users.noreply.github.com>
2021-09-18 14:50:43 +0530

    Types in TS

--- test_repo/hello.js
+++ test_repo/hello.js
@@ -9,3 +9,26 @@ var arr = [1, 22, 13];
 var arr2 = [33, 44, 55];
 var salute = "Good Morning";
 var anything = 4;
+anything = "Something";
+anything = [11, 22, 121];
+//Return types
+function again(name) {
+    console.log(name);
+}
+again("Yo");
+//Enum
+var statusCode = 2;
+var allCodes;
+(function (allCodes) {
+    allCodes[allCodes["up"] = 0] = "up";
+    allCodes[allCodes["down"] = 1] = "down";
+    allCodes[allCodes["left"] = 2] = "left";
+    allCodes[allCodes["right"] = 3] = "right";
+})(allCodes || (allCodes = {}));
+;
+if (statusCode === allCodes.left) {
+    console.log("Go left");
+}
+else {
+    console.log("change statusCode & check");
+}
--- test_repo/hello.ts
+++ test_repo/hello.ts
@@ -10,3 +10,21 @@ let salute:string="Good Morning";
 let anything:any=4;
 anything="Something";
 anything=[11,22,121];
+
+//Return types
+function again(name:string):void{
+    console.log(name);
+}
+again("Yo");
+
+//Enum
+let statusCode:number=2;
+enum allCodes{up,down,left,right};
+if(statusCode===allCodes.left)
+{
+    console.log(`Go left`)
+}
+else
+{
+    console.log("change statusCode & check")
+}
