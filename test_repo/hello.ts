774acc89c7f2e04aabeee88f4a173975990fa7bc
Rupayan <59760234+Rupayank@users.noreply.github.com>
2021-09-18 14:17:37 +0530

    Trying out TS

--- test_repo/hello.ts
+++ test_repo/hello.ts
@@ -1,4 +1,12 @@
 function greeting(person:string) {
-    return "Hello, "+person;
+    return "Hello there again, "+person;
 }
+var user="Mother";
 console.log(greeting("RK"));
+let check:boolean=true;
+let arr:number[]=[1,22,13];
+let arr2:Array<number>=[33,44,55];
+let salute:string="Good Morning";
+let anything:any=4;
+anything="Something";
+anything=[11,22,121];
